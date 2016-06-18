fb38a8fb09f5538a9547169e2e23c5f34208b787
DenisQA <Levshenkov.Denys.gmail.com>
2016-06-18 15:02:11 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+'use strict';
+ 
+var gulp        = require('gulp'),
+	sourcemaps  = require('gulp-sourcemaps'),
+	browserSync = require('browser-sync').create(),
+    sass        = require('gulp-sass');
+
+gulp.task('serve', ['sass'], function() {
+	browserSync.init({
+		server: "./",
+		port: 3010
+	});
+	gulp.watch("./stylesheet/**/*.scss", ['sass']);
+});
+ 
+gulp.task('sass', function () {
+  return gulp.src('./stylesheet/**/*.scss')
+  	.pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write('./maps'))
+    .pipe(gulp.dest('./css'))
+    .pipe(browserSync.stream());
+});
+ 
+gulp.task('default', ['serve']);
